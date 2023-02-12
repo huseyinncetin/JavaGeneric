a808e3025d66b8fe42ab38656f588f95812f987f
huseyinncetin <42445023+huseyinncetin@users.noreply.github.com>
2023-02-12 21:00:36 +0300

    Add files via upload
    Generic Method added

---
+++ GenericMethod/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String a[] ={"Şit","Şet"};
+        Integer b[]={45,86,78};
+        Character c[]={'Ç','E','T','O'};
+
+        Show.showArray(c);
+    }
+}
---
+++ GenericMethod/src/Show.java
@@ -0,0 +1,8 @@
+public class Show {
+
+    public static <T> void showArray(T[] a){
+        for(T i : a){
+            System.out.println(i);
+        }
+    }
+}
